09096e9ea1dca5853a6b91ffe37b5f70cbd40dd2
John Cook <jzc0066@auburn.edu>
2015-08-24 17:55:18 -0500

    Another contribution test

--- UAVParachuteDeployment.ino
+++ UAVParachuteDeployment.ino
@@ -16,7 +16,7 @@ boolean STATUS = true;
 
 // runs continuosly
 void loop() {
-  // insert watchdog timer
+  // insert watchdog timer 
   
   if (!checkVoltage() || !checkAcceleration()) {  // need specific order here, so as to short circuit appropriately?
     sendDeployCommand();
