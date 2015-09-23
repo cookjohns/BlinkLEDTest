97bc27894661da2064133215c3c08e9e1d0b17c0
John Cook <jzc0066@auburn.edu>
2015-09-23 15:14:32 -0500

    Setup, pin #s, comments, etc.

--- UAVParachuteDeployment.ino
+++ UAVParachuteDeployment.ino
@@ -7,19 +7,21 @@
 
 /* Pin 3 set to send parachute deployment signal */
 // CHECK THESE DUMMY VALUES
-int DEPLOYPIN  = 3;
-int VOLTAGEPIN = 5;
+int DEPLOYPIN  = 1;
+int VOLTAGEPIN = 3;
 int ACCELPIN   = 6;
 boolean PASS   = true;
 boolean FAIL   = false;
 boolean STATUS = false;
+boolean v      = true; // for voltage check inside loop
 
 // runs continuosly
 void loop() {
-  // insert watchdog timer  
+  // insert watchdog timer 
+  v = checkVoltage(); // do this here to avoid calling it twice
   
-  if (!checkVoltage() || !checkAcceleration()) {  // need specific order here, so as to short circuit appropriately?
-    sendDeployCommand(STATUS);
+  if (!v || checkAcceleration()) {
+    sendDeployCommand(v);
     // break somehow, so it doesn't continue to fire solenoid after failure?
   }
 }
@@ -27,18 +29,20 @@ void loop() {
 // the setup function runs once when you power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(6, INPUT);
 }
 
-// writes a 1 to pin 3 in order to activate solenoid to open parachute
+// sends HIGH to deploypin in order to activate solenoid to open parachute
 void sendDeployCommand(boolean statusIn) {
-  digitalWrite(DEPLOYPIN, HIGH);
+  digitalWrite(DEPLOYPIN, HIGH);  // pop chute
   if (statusIn) shutdown();  // shut down the autopilot and motors if failure mode switches flag
 }
 
 // sends a command to Ardupilot to shutdown
 void shutdown() {
-  // do stuff here
+  // trip hardware disconnect for power here
 }
 
 // returns true if voltage is above 0.3
