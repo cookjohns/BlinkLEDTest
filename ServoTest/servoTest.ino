152f93a2d2194546012b635107839ea3f95cfdeb
John Cook <jzc0066@auburn.edu>
2015-12-06 23:54:02 -0600

    Updated ServoTest to use Arduino library

--- ServoTest/servoTest.ino
+++ ServoTest/servoTest.ino
@@ -9,19 +9,29 @@ void setup()
 { 
   myservo.attach(1);  // attaches the servo on pin 1 to the servo object 
 } 
-
-void loop() 
-{ 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-    SoftwareServo::refresh();
-  } 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-    SoftwareServo::refresh();
-  } 
+int main() {
+//  for(pos = 0; pos < 90; pos += 1)  // goes from 0 degrees to 160 degrees 
+//   {                                  // in steps of 1 degree 
+//      myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//      delay(15);                       // waits 15ms for the servo to reach the position 
+//      SoftwareServo::refresh();
+//    } 
+  myservo.write(90);
+  return 0;
 }
+
+//void loop() 
+//{ 
+//  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+//  {                                  // in steps of 1 degree 
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//    delay(15);                       // waits 15ms for the servo to reach the position 
+//    SoftwareServo::refresh();
+//  } 
+//  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+//  {                                
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//    delay(15);                       // waits 15ms for the servo to reach the position 
+//    SoftwareServo::refresh();
+//  } 
+//}
