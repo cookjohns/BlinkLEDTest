4cd88faa85e0e93958e9cbf3a953879dac938f07
John Cook <jzc0066@auburn.edu>
2015-12-16 13:14:19 -0600

    Removed unnecessary code; Comments.

--- ServoTest/main.c
+++ ServoTest/main.c
@@ -6,8 +6,10 @@ int main() {
     PLLCSR |= (1 << PLLE) | (1 << PCKE);
 
     // Set prescaler to PCK/2048
+    // (Setting CS (clock select) to 3)
     TCCR1 |= (1 << CS10) | (1 << CS11) | (0 << CS12) | (0 << CS13);
 
+    // Output Compare Register
     OCR1B = 128;
     OCR1C = 255;
 
@@ -15,8 +17,6 @@ int main() {
     DDRB  |= (1 << PB4);
     GTCCR |= (1 << COM1B0) | (1 << COM1B1);
     GTCCR |= (1 << PWM1B);
-
-    TCCR1 |= (1 << COM1A0);
         
 	while (1) {} // run infinitely
 
