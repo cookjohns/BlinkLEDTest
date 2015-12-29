f1b366a1fdf9fbbe93002f3c297fb583cf87e7c5
John Cook <jzc0066@auburn.edu>
2015-12-29 13:08:28 -0600

    Pushed Arduino servo test

--- ServoTest/main.c
+++ ServoTest/main.c
@@ -3,21 +3,23 @@
 
 int main() {
     
-    PLLCSR |= (1 << PLLE) | (1 << PCKE);
+    //PLLCSR |= (1 << PLLE) | (1 << PCKE);
 
     // Set prescaler to PCK/2048
     // (Setting CS (clock select) to 3)
-    TCCR1 |= (1 << CS10) | (1 << CS11) | (0 << CS12) | (0 << CS13);
+    //TCCR1 |= (1 << CS10) | (1 << CS11) | (0 << CS12) | (0 << CS13);
 
     // Output Compare Register
-    OCR1B = 128;
-    OCR1C = 255;
+    //OCR1B = 128;
+    //OCR1C = 255;
 
     // Enable OCRB output on PB4, configure compare mode and enable PWM B
-    DDRB  |= (1 << PB4);
-    GTCCR |= (1 << COM1B0) | (1 << COM1B1);
-    GTCCR |= (1 << PWM1B);
-        
+    //DDRB  |= (1 << PB4);
+    //GTCCR |= (1 << COM1B0) | (1 << COM1B1);
+    //GTCCR |= (1 << PWM1B);
+    
+    //TCCR1 |= (1 << COM1A0);
+    
 	while (1) {} // run infinitely
 
 	return 0;
